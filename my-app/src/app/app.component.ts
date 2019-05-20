93e17d18b1d3dc475f80d4051351bd2be83fb2e4
MichałNawrot <michal.nawrot.mn.mn@gmail.com>
2019-05-20 10:36:16 +0200

    male zmiany

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { $$ } from 'protractor';
+import { build$$ } from 'protractor/built/element';
+import { element } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,9 @@ export class AppComponent {
   clickCounter = 0;
   number = 0;
   allMarked = true;
+  el;
 
   startFunction() {
-    this.clickCounter++;
     this.number = Math.floor(Math.random() * 4) + 1;
 
     document.getElementById('class1').style.visibility = "hidden";
@@ -40,36 +42,50 @@ export class AppComponent {
     }
   }
 
-  /*
-    $("td").click(function (e) {
-      if (!(this.hasClass("empty")) {
-        $(this).toggleClass("marked");
-        if (!$(this).next().hasClass("empty")) $(this).next().toggleClass("marked");
-        if (!$(this).prev().hasClass("empty")) $(this).prev().toggleClass("marked");
+  clickFunction() {
+    this.clickCounter++;
+    /*
+        $(this).toggleClass("marked"); //zamien na class "marked"
+        
+        if (!$(this).next().hasClass("empty")) $(this).next().toggleClass("marked"); // jezli nastepne nie ma klasy "empty" to zmien na "marked"
+        if (!$(this).prev().hasClass("empty")) $(this).prev().toggleClass("marked"); // jezli poprzednie nie ma klasy "empty" to zmien na "marked"
 
-        let fieldAbove = $(this).parent().prev().children().eq($(this).index());
-        if (!fieldAbove.hasClass("empty")) fieldAbove.toggleClass("marked");
+        let fieldAbove = $(this).parent().prev().children().eq($(this).index());     // dziecko o tym samym indexie poprzedniego tr
+        if (!fieldAbove.hasClass("empty")) fieldAbove.toggleClass("marked");         // jezli gora nie ma klasy "empty" to zmien na "marked"
 
-        let fieldBelow = $(this).parent().next().children().eq($(this).index());
-        if (!fieldBelow.hasClass("empty")) fieldBelow.toggleClass("marked");
+        let fieldBelow = $(this).parent().next().children().eq($(this).index());    // dziecko o tym samym indexie nastepnego tr
+        if (!fieldBelow.hasClass("empty")) fieldBelow.toggleClass("marked");        // jezli dol nie ma klasy "empty" to zmien na "marked"
+    */
 
-        clicks++;
-        $("#clicks").text(clicks);
+    //dla testu 
+    if (this.clickCounter > 5) {
+      if (this.clickCounter % 2) this.allMarked = false; 
+      else this.allMarked = true;
+    }
 
-        $(level.name + " td").each(function () {
-          if ($(this).hasClass("marked") || $(this).hasClass("empty")) { }
-          else this.allMarked = false;
-        });
+    /*
+    if(document.getElementsByTagName("td")){                             // sprawdz dla kazdego td
+      if ($(this).hasClass("marked") || $(this).hasClass("empty")) { }   // jesli jest marked lub empty nic nie rob
+    else this.allMarked = false;)                                        // inaczej ustaw allMarked na false
+    }
+    */
 
-        if (this.allMarked) {
-          document.getElementById('default-title').style.visibility = "hidden";
-          document.getElementById('win-title').style.visibility = "visible";
-          $("td").each(function () {
-            $(this).off("click");
-          });
-        }
-      }
-    });*/
+    /*
+    this.el = document.getElementById("td");
+
+    if () {
+      if (element(el).hasClass("marked") || element(el).hasCLass("empty")) { }
+      else this.allMarked = false;
+    }
+    */
+
+
+    if (this.allMarked) {
+      document.getElementById("default-title").style.visibility = "hidden";
+      document.getElementById("win-title").style.visibility = "visible";
+      // click().block                                                   // wylacz mozliwosc klikania na calej stronie/na kazdy tr
+    }
+  }
 }
 
 
